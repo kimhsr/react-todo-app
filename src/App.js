bce9aceff30ca5449b32b7069fcb664f24f57ff9
kimhsr <kimhsr@naver.com>
2022-12-06 01:31:36 +0900

    할 일 목록 추가하기 기능 구현

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
         completed: false,
       },
     ],
+    value: "",
   };
 
   btnStyle = {
@@ -40,6 +41,26 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData });
   };
 
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    // form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌
+    e.preventDefault();
+
+     // 새로운 할 일 데이터
+     let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [ ...this.state.todoData, newTodo ] });
+
+  };
+
   render() {
     return (
       <div className="container">
@@ -60,6 +81,23 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
         </div>
       </div>
     );
