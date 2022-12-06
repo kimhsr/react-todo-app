97d3d217f5f780d0368bc4ba073ec18484e43be5
kimhsr <kimhsr@naver.com>
2022-12-07 01:34:30 +0900

    checkbox, data.title 간격 수정

--- src/components/List.js
+++ src/components/List.js
@@ -26,13 +26,18 @@ export default function List({ todoData, setTodaData }) {
                 type="checkbox"
                 defaultChecked={false}
                 onChange={() => handleCompleChange(data.id)}
-              />
+              />{" "}
               <span className={data.completed ? "line-through" : undefined}>
                 {data.title}
               </span>
             </div>
             <div className="items-center">
-              <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
+              <button
+                className="px-4 py-2 float-right"
+                onClick={() => handleClick(data.id)}
+              >
+                x
+              </button>
             </div>
           </div>
         </div>
