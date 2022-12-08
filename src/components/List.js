87e5bf6063f46f8a38c32a60e0788a03b323b515
kimhsr <kimhsr@naver.com>
2022-12-08 15:47:07 +0900

    List 컴포넌트 생성

---
+++ src/components/List.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const List = ({
+  id,
+  title,
+  completed,
+  provided,
+  todoData,
+  setTodaData,
+  snapshot,
+}) => {
+  const handleCompleChange = (id) => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodaData(newTodoData);
+  };
+
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    setTodaData(newTodoData);
+  };
+  return (
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+    >
+      <div className="items-center">
+        <input
+          type="checkbox"
+          defaultChecked={false}
+          onChange={() => handleCompleChange(id)}
+        />{" "}
+        <span className={completed ? "line-through" : undefined}>{title}</span>
+      </div>
+      <div className="items-center">
+        <button
+          className="px-4 py-2 float-right"
+          onClick={() => handleClick(id)}
+        >
+          x
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,22 +1,9 @@
+import { comment } from "postcss";
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
 
-export default function List({ todoData, setTodaData }) {
-  const handleCompleChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodaData(newTodoData);
-  };
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodaData(newTodoData);
-  };
-
+export default function Lists({ todoData, setTodaData }) {
   const handleEnd = (result) => {
     if (!result.destination) return;
 
@@ -44,38 +31,16 @@ export default function List({ todoData, setTodaData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <List
                       key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={false}
-                          onChange={() => handleCompleChange(data.id)}
-                        />{" "}
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2 float-right"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          x
-                        </button>
-                      </div>
-                    </div>
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      provided={provided}
+                      todoData={todoData}
+                      setTodaData={setTodaData}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
